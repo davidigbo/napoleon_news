b66fa51dadd1555a8d708ef01b98296717b6934f
david30-maker <davidokpah396@gmail.com>
2025-01-29 12:45:11 +0100

    modify

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,12 +1,4 @@
 class Vote < ApplicationRecord
   belongs_to :contestant
   belongs_to :voter
-
-  after_create :broadcast_leaderborad_update
-
-  private
-
-  def broadcast_leaderborad_update
-    ActionCable.server.broadcast("leaderboard_#{contest_id}", { message: "New vote cast!", contestants: Contestant.left_joins(:votes).group(:id).order("count(votes.id) DESC")})
-  end
 end
