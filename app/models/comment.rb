ddb58b0af0c8f39490e3db305e48b979546e13fc
david30-maker <davidokpah396@gmail.com>
2025-01-29 11:33:05 +0100

    modify comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :commentable, polymorphic: true
 
-  enum :comment_type, [:review_comment: 0, :reader_comment: 1]
+  enum :comment_type, [review_comment: 0, reader_comment: 1]
 
   validates :content, presence: true
 end
