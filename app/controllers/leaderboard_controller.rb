139d0b5b9864b53ed2a9c48595a53bcae137c48a
david30-maker <davidokpah396@gmail.com>
2025-01-29 11:49:46 +0100

    create leaderboard controller

---
+++ app/controllers/leaderboard_controller.rb
@@ -0,0 +1,21 @@
+class LeaderboardController < ApplicationController
+
+    def index
+        @contestants = Contestant.left_joins(:votes)
+                                  .group(:id)
+                                    .order("COUNT(votes.id) DESC")
+
+        @votes = Vote.all
+
+        @contestants.each do |contestant|
+            contestant.votes_count = contestant.votes.count
+
+            contestant.save
+        end
+
+        respond_to do |format|
+            format.html
+            format.json { render json: @contestants }
+        end
+    end
+end
