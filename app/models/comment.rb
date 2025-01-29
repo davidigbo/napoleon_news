bea85e049063f71ce8cf42c362693b443bb8dd7d
david30-maker <davidokpah396@gmail.com>
2025-01-29 11:13:59 +0100

    modify

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  belongs_to :article
+  belongs_to :commentable, polymorphic: true
 
-  enum :comment_type, [:review_comment, :reader_comment]
+  enum :comment_type, [:review_comment: 0, :reader_comment: 1]
+
+  validates :content, presence: true
 end
--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -4,11 +4,23 @@ class Contestant < ApplicationRecord
   belongs_to :approved_by, class_name: 'User', optional: true
 
   has_one_attached :image
-  
+
+  has_one_attached :description
+
+  validates :description, presence: true
   validates :image, content_type: [:png, :jpg, :jpeg]
 
   enum approved: { pending: 0, approved: 1, rejected: 2 }
 
   extend FriendlyId
   friendly_id :name, use: :slugged
+
+  before_update :set_approved_at, if: :approved_changed?
+
+  private
+
+  def set_approved_at
+    self.approved_at = Time.current if approved === "approved" && approved_at.nil?
+    self.approved_by_id = user.id if approved === "approved" && approved_by_id.nil?
+  end
 end
