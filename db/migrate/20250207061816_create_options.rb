12cf2aa0436607cdcc649a0e8e1f0ad8d73e1388
David Igbo <davidokpah396@gmail.com>
2025-02-07 07:18:37 +0100

    create option model

---
+++ db/migrate/20250207061816_create_options.rb
@@ -0,0 +1,11 @@
+class CreateOptions < ActiveRecord::Migration[7.2]
+  def change
+    create_table :options do |t|
+      t.string :content
+      t.boolean :is_correct
+      t.references :question, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
