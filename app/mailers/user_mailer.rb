146b573377b2868bb012ab77b2f23cd282163f17
Moses Ogwo <mosesogwo@gmail.com>
2025-02-13 02:31:13 +0100

    add contest mailers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,13 @@
 class UserMailer < ApplicationMailer
+  layout 'mailer'
+
   def welcome_email(user)
+    @user = user
+    attachments.inline['icon.png'] = File.read(Rails.root.join('public', 'icon.png'))
     mail(
       from: 'no-reply@napoleonnews.com',
-      to: user.email,
+      to: @user.email,
       subject: 'Welcome to Our Platform',
-      body: 'Thanks for signing up!'
     )
   end
 end
