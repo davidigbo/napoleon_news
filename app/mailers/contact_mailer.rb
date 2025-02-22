0e6b2591f18378edeb9f11bf07c4e910eb4dc9b1
Moses Ogwo <mosesogwo@gmail.com>
2025-02-22 19:19:51 +0100

    update contact form submission

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,6 +6,6 @@ class ContactMailer < ApplicationMailer
     @email = email
     @message = message
 
-    mail(from: email, subject: "New Contact Form Submission from #{@name}")
+    mail(from: 'no-reply@napoleonnews.com', subject: "New Contact Form Submission from #{@name}")
   end
 end
