4f9f65415af28a5e55266d9b5f4a4acc14ddb772
Moses Ogwo <mosesogwo@gmail.com>
2025-05-20 18:01:46 +0100

    add bio to users, provide edit profile link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :bio])
   end
 
   def set_nav_link_variables
