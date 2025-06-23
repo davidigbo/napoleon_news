03cf8f5092e17236892219235935101d9acae5aa
Moses Ogwo <mosesogwo@gmail.com>
2025-06-23 13:56:52 +0100

    update

--- app/controllers/review_article_controller.rb
+++ app/controllers/review_article_controller.rb
@@ -2,9 +2,11 @@ class ReviewArticleController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    head :unauthorized unless current_user&.admin? || current_user&.editor?
-
     @article = Article.includes(:comments).friendly.find(params[:slug])
+    
+    head :unauthorized unless current_user&.admin? || current_user&.editor? || current_user == @article.author
+
+    
     article_image = @article.image
     @article_image_url = article_image.present? ? url_for(article_image) : nil
     # @article_body = @article_image_url.present? ? @article.body_without_images : @article.body
